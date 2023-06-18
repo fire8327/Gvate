2051ed4a3d33447108417d9e964190c2371ac706
fire8327 <114702513+fire8327@users.noreply.github.com>
2023-06-18 15:50:17 +0300

    last upd

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,30 @@ $(".dropdown").each((i,el)=>{
         list.toggleClass("shadow-[0_0_20px_-13px]")
     })
 })
+
+
+/* observer width */
+let width = $(window).width()
+$(window).on("load resize", () => {
+    width = $(window).width()
+    if(width < 1024) {
+        let el = $(".el")
+        for(let i = 6; i < el.length; i++) {
+            $(el[i]).addClass("max-lg:hidden")
+        }
+        let count = 6;
+        $("#load").on("click", ()=>{
+            if(count < 24) {
+                count += 6
+            } else {
+                count +=1
+            }       
+            console.log(count);         
+            if(count <= el.length) {
+                for(let i = 0; i < count; i++) {
+                    $(el[i]).removeClass("max-lg:hidden")
+                }
+            }
+        })
+    }
+})
